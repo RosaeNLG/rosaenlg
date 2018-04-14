854c3a2cba103a8c52496a5aa3e4a480366c7033
Ludan Stoecklé <ludan.stoeckle@addventa.com>
2018-04-14 19:45:17 +0200

    new test

--- packages/freenlg/test-freenlg/testRandom.js
+++ packages/freenlg/test-freenlg/testRandom.js
@@ -28,9 +28,9 @@ function testClassicDistribution() {
   var distrib = getDistrib(iter, range, {}, []);
 
   for (var k in distrib) {
-    var proportion = iter / distrib[k];
+    var proportion = distrib[k] / iter;
     it(`classic distribution: proportion of ${k}: ${proportion}`, () => it.eq(true, 
-      proportion > range*0.9 && proportion < range*1.1
+      proportion > 1/range*0.9 && proportion < 1/range*1.1
     ) );    
   }
 };
@@ -45,30 +45,51 @@ function testDistributionWithExcluded(range, excluded) {
 
   var realRange = range - excluded.length;
   for (var k in distrib) {
-    var proportion = iter / distrib[k];
+    var proportion = distrib[k] / iter;
     it(`proportion of ${k}: ${proportion}`, () => it.eq(true, 
-      proportion > realRange*0.9 && proportion < realRange*1.1
+      proportion > 1/realRange*0.9 && proportion < 1/realRange*1.1
     ) );
   }
 
 };
 
-function testDistributionWithWeights(range, weights) {
-  var randomManager = getRandomManager();
+function getItemWeight(params, item) {
+  return ( params[`${item}`] ? params[`${item}`].weight : null ) || 1;
+};
+function getTotalWeights(params) {
+  var sumOfWeights = 0;
+  for (var k in params) {
+    sumOfWeights += getItemWeight(params, k);
+  }
+  return sumOfWeights;  
+};
 
+function testDistributionWithWeights(range, weights) {
+  const iter = 10000;
+  var distrib = getDistrib(iter, range, weights, []);
+
+  var sumOfWeights = getTotalWeights(weights);
+  for (var k in distrib) {
+    var proportion = distrib[k] / iter;
+    var weight = getItemWeight(weights, k);
+    var expectedProp = weight / sumOfWeights;
+    it(`weighted distribution: proportion of ${k}: ${proportion}, weight is ${weight} / totalw is ${sumOfWeights}`, () => it.eq(true, 
+      proportion > expectedProp*0.9 && proportion < expectedProp*1.1
+    ) );    
+  }
 
 };
 
 module.exports = it => {
   testClassicDistribution();
-  
   testDistributionWithExcluded(20, [11,12,13,14,15,16,17,18,19,20]);
   testDistributionWithExcluded(20, [1,3,5,7,9,11,13,15,17,19]);
   testDistributionWithExcluded(4, [1,2,3]);
 
+  testDistributionWithWeights(10, { '2': {weight: 5} });
+  /*
 
-  // testDistributionWithWeights(5, { '2': {weight: 5} });
-
+  */
   // testDistributionWithWeightsAndExcluded
 
 
