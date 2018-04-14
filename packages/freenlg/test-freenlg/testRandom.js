d1f43752a96f41a2cd33aeef5e5004e68f620df2
Ludan Stoecklé <ludan.stoeckle@addventa.com>
2018-04-14 19:27:55 +0200

    refacto tests

--- packages/freenlg/test-freenlg/testRandom.js
+++ packages/freenlg/test-freenlg/testRandom.js
@@ -3,21 +3,30 @@ const NlgLib = require("freenlg-core").NlgLib;
 
 var it = junit();
 
-
-function testClassicDistribution() {
+function getRandomManager() {
   var nlgLib = new NlgLib({
     language: 'en_US',
     forceRandomSeed: 1
   });
-  var randomManager = nlgLib.randomManager;
+  return nlgLib.randomManager;
+};
 
-  const iter = 10000;
-  const range = 10;
+function getDistrib(iter, range, params, excluded) {
+  var randomManager = getRandomManager();
   var distrib = {};
   for (var i=0; i<iter; i++) {
-    var rnd = randomManager.randomNotIn(range, {}, []);
+    var rnd = randomManager.randomNotIn(range, params, excluded);
     distrib[rnd] = distrib[rnd]+1 || 1;
   }
+  return distrib;
+};
+
+function testClassicDistribution() {
+  
+  const iter = 10000;
+  const range = 10;
+  var distrib = getDistrib(iter, range, {}, []);
+
   for (var k in distrib) {
     var proportion = iter / distrib[k];
     it(`classic distribution: proportion of ${k}: ${proportion}`, () => it.eq(true, 
@@ -27,21 +36,11 @@ function testClassicDistribution() {
 };
 
 function testDistributionWithExcluded(range, excluded) {
-  var nlgLib = new NlgLib({
-    language: 'en_US',
-    forceRandomSeed: 1
-  });
-  var randomManager = nlgLib.randomManager;
-
   const iter = 10000;
-  var distrib = {};
-  for (var i=0; i<iter; i++) {
-    var rnd = randomManager.randomNotIn(range, {}, excluded);
-    distrib[rnd] = distrib[rnd]+1 || 1;
-  }
-  
+  var distrib = getDistrib(iter, range, {}, excluded);
+
   excluded.map(ex => {
-    it(`${ex} is excluded`, () => it.eq(true, distrib[ex]==null) )
+    it(`${ex} is excluded: ${distrib[ex]}`, () => it.eq(true, distrib[ex]==null) )
   });
 
   var realRange = range - excluded.length;
@@ -54,11 +53,24 @@ function testDistributionWithExcluded(range, excluded) {
 
 };
 
+function testDistributionWithWeights(range, weights) {
+  var randomManager = getRandomManager();
+
+
+};
+
 module.exports = it => {
   testClassicDistribution();
+  
   testDistributionWithExcluded(20, [11,12,13,14,15,16,17,18,19,20]);
   testDistributionWithExcluded(20, [1,3,5,7,9,11,13,15,17,19]);
   testDistributionWithExcluded(4, [1,2,3]);
+
+
+  // testDistributionWithWeights(5, { '2': {weight: 5} });
+
+  // testDistributionWithWeightsAndExcluded
+
 
 };
 
