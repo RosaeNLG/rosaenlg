afd6728cb027d1bc5eee452a9660c54a2bbc244c
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2019-01-06 14:45:46 +0100

    det German test
    Former-commit-id: 597d4ab980f8e4838e2f7e448d2f9444acb46e9e

--- packages/freenlg-core/src/Determinant.ts
+++ packages/freenlg-core/src/Determinant.ts
@@ -1,16 +1,43 @@
 
 import { getGenderFrenchWord } from "./FrenchWordsGender";
+import { getGenderGermanWord } from "./GermanWordsGenderCases";
 
 export function getDet(lang: string, det: string, obj: string, params: any): string {
   if (lang=='en_US') {
     return "TODO_DET_en_US";
-  } else if (lang=='de_DE') {
-    return "TODO_DET_de_DE";
 
+  } else if (lang=='de_DE') {
+    var gender:string = getGenderGermanWord(obj);
+    if (gender==null) {
+      console.log(`ERROR cannot put an article on ${obj}, gender is not in German dict`);
+      return '';
+    }
+    // console.log(gender);
+    var res: string = '';
+    switch(det) {
+      case 'DEFINITE':
+        switch(gender) {
+          case 'M':
+            res = 'der';
+            break;
+          case 'F':
+            res = 'die';
+            break;
+          case 'N':
+            res = 'das';
+            break;
+        }
+        break;
+      default:
+        console.log(`ERROR ${det} is not supported in de_DE`);
+    }
+    return `${res} `;
+
+    
   } else if (lang=='fr_FR') {
     var gender:string = getGenderFrenchWord(obj);
     if (gender==null) {
-      console.log(`ERROR cannot put an article on ${obj}, gender is not in dict`);
+      console.log(`ERROR cannot put an article on ${obj}, gender is not in French dict`);
       return '';
     }
     var res: string = '';
@@ -24,7 +51,6 @@ export function getDet(lang: string, det: string, obj: string, params: any): str
       default:
         console.log(`ERROR ${det} is not supported in fr_FR`);
     }
-
     return `${res} `;
   }
 }
