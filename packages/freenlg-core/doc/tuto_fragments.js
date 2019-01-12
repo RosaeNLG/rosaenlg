957c9ff06f84469acaf4948104436d631b6112fe
Ubuntu <ubuntu@ip-172-31-9-195.eu-west-1.compute.internal>
2019-01-12 11:27:30 +0000

    doc debug
    Former-commit-id: b0c03a03458d9ebbb39797079cbdedba1804db77

--- packages/freenlg-core/doc/tuto_fragments.js
+++ packages/freenlg-core/doc/tuto_fragments.js
@@ -31,42 +31,16 @@ var phones = [
 ];
 // end::data[]
 
-// tag::mainLoop_fr_FR[]
+// tag::mainLoop[]
 for (var i=0; i<phones.length; i++) {
 
   var res = freenlgPug.renderFile('tuto.pug', {
-      language: 'fr_FR',
+      language: '{freenlg_lang}',
       phone: phones[i],
       cache: true
   });
   console.log( res );
 
 }
-// end::mainLoop_fr_FR[]
-
-// tag::mainLoop_en_US[]
-for (var i=0; i<phones.length; i++) {
-
-  var res = freenlgPug.renderFile('tuto.pug', {
-      language: 'en_US',
-      phone: phones[i],
-      cache: true
-  });
-  console.log( res );
-
-}
-// end::mainLoop_en_US[]
-
-// tag::mainLoop_de_DE[]
-for (var i=0; i<phones.length; i++) {
-
-  var res = freenlgPug.renderFile('tuto.pug', {
-      language: 'de_DE',
-      phone: phones[i],
-      cache: true
-  });
-  console.log( res );
-
-}
-// end::mainLoop_de_DE[]
+// end::mainLoop[]
 
