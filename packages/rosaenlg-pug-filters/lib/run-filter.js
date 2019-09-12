183903459b01efdc0f2da54a179b3ecb676157a3
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2019-09-12 15:50:31 +0200

    solve uglify pb with 1.0.1
    Former-commit-id: ea24107e12c3be250143405264f4b0fd8710e051

--- packages/rosaenlg-pug-filters/lib/run-filter.js
+++ packages/rosaenlg-pug-filters/lib/run-filter.js
@@ -1,17 +1,17 @@
 'use strict';
 
-var jstransformer = require('jstransformer');
-var uglify = require('uglify-js');
-var CleanCSS = require('clean-css');
-var resolve = require('resolve');
+const jstransformer = require('jstransformer');
+//const uglify = require('uglify-js');
+const CleanCSS = require('clean-css');
+const resolve = require('resolve');
 
 module.exports = filter;
 function filter(name, str, options, currentDirectory, funcName) {
   funcName = funcName || 'render';
-  var tr;
+  let tr;
   try {
     try {
-      tr = require(resolve.sync('jstransformer-' + name, {basedir: currentDirectory || process.cwd()}));
+      tr = require(resolve.sync('jstransformer-' + name, { basedir: currentDirectory || process.cwd() }));
     } catch (ex) {
       tr = require('jstransformer-' + name);
     }
@@ -19,13 +19,15 @@ function filter(name, str, options, currentDirectory, funcName) {
   } catch (ex) {}
   if (tr) {
     // TODO: we may want to add a way for people to separately specify "locals"
-    var result = tr[funcName](str, options, options).body;
+    let result = tr[funcName](str, options, options).body;
     if (options && options.minify) {
       try {
         switch (tr.outputFormat) {
+          /*
           case 'js':
-            result = uglify.minify(result, {fromString: true}).code;
+            result = uglify.minify(result, { fromString: true }).code;
             break;
+          */
           case 'css':
             result = new CleanCSS().minify(result).styles;
             break;
@@ -36,7 +38,7 @@ function filter(name, str, options, currentDirectory, funcName) {
     }
     return result;
   } else {
-    var err = new Error('unknown filter ":' + name + '"');
+    const err = new Error('unknown filter ":' + name + '"');
     err.code = 'UNKNOWN_FILTER';
     throw err;
   }
