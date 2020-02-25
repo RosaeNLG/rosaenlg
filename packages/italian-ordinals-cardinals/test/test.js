1338d957a43664d12c5a1f7b6c7150c341a8daca
Marco Riva <marco.riva@radicalbit.io>
2020-02-25 18:01:32 +0100

    fix test
    Former-commit-id: 1a51c3fdc807ea4bf401f0fe5857ab24c16afe68

--- packages/italian-ordinals-cardinals/test/test.js
+++ packages/italian-ordinals-cardinals/test/test.js
@@ -3,10 +3,21 @@ const lib = require('../dist/index.js');
 
 const testCasesOrdinals = [
   [1, 'primo'],
-  [66, 'sessantiseisimo'],
+  [23, 'ventitreesimo'],
+  [61, 'sessantunesimo'],
+  [66, 'sessantaseiesimo'],
   [100, 'centesimo'],
+  [385, 'trecentottantacinquesimo'],
   [1000, 'millesimo'],
-  [1000000, 'millionesimo'],
+  [1131, 'millecentotrentunesimo'],
+  [1234, 'milleduecentotrentaquattresimo'],
+  [1456, 'millequattrocentocinquantaseiesimo'],
+  [9453, 'novemilaquattrocentocinquantatreesimo'],
+  [20000, 'ventimillesimo'],
+  [31298, 'trentunomiladuecentonovantottesimo'],
+  [141232, 'centoquarantunomiladuecentotrentaduesimo'],
+  [954322, 'novecentocinquantaquattromilatrecentoventiduesimo'],
+  [1000000, 'milionesimo'],
 ];
 
 describe('italian-ordinals-cardinals', function() {
@@ -18,7 +29,7 @@ describe('italian-ordinals-cardinals', function() {
       });
     }
     it(`out of bound`, function() {
-      assert.throws(() => lib.getOrdinal(333), /found/);
+      assert.throws(() => lib.getOrdinal(1000770), /found/);
     });
   });
 });
