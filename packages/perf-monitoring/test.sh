5a45df5cf1537fd9f9cb844320bb2a6e52b1a347
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2020-12-10 07:49:39 +0100

    performance monitoring debug
    Signed-off-by: Ludan Stoeckle <ludan.stoeckle@gmail.com>

--- packages/perf-monitoring/test.sh
+++ packages/perf-monitoring/test.sh
@@ -4,9 +4,13 @@ BRANCH="$(git rev-parse --abbrev-ref HEAD)"
 
 echo branch: $BRANCH
 
-echo existing files:
+pwd
 
+echo existing files:
 ls -l benchmark-*.json
 
 # if master, will override previous once done
 npx bipbip --compare benchmark-master.json --save benchmark-${BRANCH}.json
+
+echo existing files, after:
+ls -l benchmark-*.json
