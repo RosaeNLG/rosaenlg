4a6f8150760634d083a3c035be18095893f88370
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2020-12-25 18:11:50 +0100

    symlink log
    Signed-off-by: Ludan Stoeckle <ludan.stoeckle@gmail.com>

--- packages/generator-rosaenlg/symlink.js
+++ packages/generator-rosaenlg/symlink.js
@@ -1,12 +1,23 @@
 const path = require('path');
 const fs = require('fs');
 
-fs.unlink('node_modules/rosaenlg-boilerplate', () => {
-  // we don't care
-});
+function createSymlink() {
+  fs.mkdir('node_modules', { recursive: true }, () => {
+    fs.unlink('node_modules/rosaenlg-boilerplate', () => {
+      fs.symlink(
+        path.join(__dirname, '../rosaenlg-boilerplate/'),
+        'node_modules/rosaenlg-boilerplate',
+        'junction',
+        (err) => {
+          if (err) {
+            console.log('could not create symlink:', err);
+          } else {
+            console.log('symlink created');
+          }
+        },
+      );
+    });
+  });
+}
 
-fs.symlink(path.join(__dirname, '../rosaenlg-boilerplate/'), 'node_modules/rosaenlg-boilerplate', 'junction', (err) => {
-  if (err) {
-    console.log('could not create symlink:', err);
-  }
-});
+return createSymlink();
