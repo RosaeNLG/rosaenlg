844d951c686558fc69fd893289bbaf3bc4802b39
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2023-07-01 09:14:06 +0200

    italian-determiners
    Signed-off-by: Ludan Stoeckle <ludan.stoeckle@gmail.com>

--- packages/italian-determiners/src/index.ts
+++ packages/italian-determiners/src/index.ts
@@ -74,6 +74,14 @@ export type Numbers = 'S' | 'P';
 export type Dist = 'NEAR' | 'FAR';
 export type DetType = 'DEFINITE' | 'INDEFINITE' | 'DEMONSTRATIVE'; // 'POSSESSIVE' not supported today
 
+type GenderNumberIndex = 'MS' | 'MP' | 'FS' | 'FP';
+interface GenderNumber {
+  MS?: string;
+  MP?: string;
+  FS?: string;
+  FP?: string;
+}
+
 export function getDet(detType: DetType, gender: Genders, number: Numbers, dist: Dist): string {
   if (detType != 'DEFINITE' && detType != 'INDEFINITE' && detType != 'DEMONSTRATIVE') {
     const err = new Error();
@@ -113,18 +121,18 @@ export function getDet(detType: DetType, gender: Genders, number: Numbers, dist:
         err.message = `dist must be NEAR or FAR, here ${dist}`;
         throw err;
       }
-      const dem = {
+      const dem: { [index: string]: GenderNumber } = {
         NEAR: { MS: 'questo', MP: 'questi', FS: 'questa', FP: 'queste' },
         FAR: { MS: 'quello', MP: 'quelli', FS: 'quella', FP: 'quelle' },
       };
-      return dem[dist][gender + number];
+      return (dem[dist] as GenderNumber)[(gender + number) as GenderNumberIndex] as string;
 
     case 'DEFINITE':
     case 'INDEFINITE':
-      const dets = {
+      const dets: { [index: string]: GenderNumber } = {
         DEFINITE: { MS: 'il', MP: 'i', FS: 'la', FP: 'le' },
         INDEFINITE: { MS: 'un', FS: 'una' },
       };
-      return dets[detType][gender + number];
+      return (dets[detType] as GenderNumber)[(gender + number) as GenderNumberIndex] as string;
   }
 }
