4a497c1255dd60c89c21b7145a68853605c071a4
Ludan Stoeckle <ludan.stoeckle@gmail.com>
2023-07-02 09:59:11 +0200

    spanish-adjectives-wrapper
    Signed-off-by: Ludan Stoeckle <ludan.stoeckle@gmail.com>

--- packages/spanish-adjectives-wrapper/src/index.ts
+++ packages/spanish-adjectives-wrapper/src/index.ts
@@ -6,26 +6,37 @@
 
 import { Genders, Numbers, agreeAdjective as agreeAdjectiveFct } from 'spanish-adjectives';
 
+type AdjectiveInfoKey = 'MStrue' | 'MPtrue' | 'FStrue' | 'FPtrue' | 'MSfalse' | 'MPfalse' | 'FSfalse' | 'FPfalse';
+
 export interface AdjectiveInfo {
-  MStrue: string;
-  MPtrue: string;
-  FStrue: string;
-  FPtrue: string;
-  MSfalse: string;
-  MPfalse: string;
-  FSfalse: string;
-  FPfalse: string;
+  MStrue: string | null;
+  MPtrue: string | null;
+  FStrue: string | null;
+  FPtrue: string | null;
+  MSfalse: string | null;
+  MPfalse: string | null;
+  FSfalse: string | null;
+  FPfalse: string | null;
 }
 export interface AdjectivesInfo {
   [key: string]: AdjectiveInfo;
 }
 
-function getKey(gender: Genders, number: Numbers, precedesNoun?: boolean): string {
-  return `${gender}${number}${precedesNoun ? 'true' : 'false'}`;
+function getKey(gender: Genders, number: Numbers, precedesNoun?: boolean): AdjectiveInfoKey {
+  return `${gender}${number}${precedesNoun ? 'true' : 'false'}` as AdjectiveInfoKey;
 }
 
 export function getAdjectiveInfo(adjective: string): AdjectiveInfo {
-  const res = {};
+  const res: AdjectiveInfo = {
+    MStrue: null,
+    MPtrue: null,
+    FStrue: null,
+    FPtrue: null,
+    MSfalse: null,
+    MPfalse: null,
+    FSfalse: null,
+    FPfalse: null,
+  };
   for (const gender of ['M', 'F']) {
     for (const number of ['S', 'P']) {
       for (const precedes of [true, false]) {
@@ -38,7 +49,7 @@ export function getAdjectiveInfo(adjective: string): AdjectiveInfo {
       }
     }
   }
-  return res as AdjectiveInfo;
+  return res;
 }
 
 export function agreeAdjective(
@@ -51,7 +62,7 @@ export function agreeAdjective(
   if (adjectivesInfo) {
     const key = getKey(gender, number, precedesNoun);
     if (adjectivesInfo[adjective] && adjectivesInfo[adjective][key]) {
-      return adjectivesInfo[adjective][key];
+      return (adjectivesInfo[adjective] as AdjectiveInfo)[key] as string;
     }
   }
   return agreeAdjectiveFct(adjective, gender, number, precedesNoun);
